2ec55a0756a3d4f1881d5ae2ba760ff86bd25cef
sumedha-95 <sumedhalakmal95@gmail.com>
2023-01-21 19:46:21 +0530

    revoke account

--- server/server.js
+++ server/server.js
@@ -28,14 +28,10 @@ app.post("/", async (req, res) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
-      // Higher values means the model will take more risks.
       temperature: 0,
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      // alternative to sampling with temperature, called nucleus sampling
+      max_tokens: 3000,
       top_p: 1,
-      // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       frequency_penalty: 0.5,
-      // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
       presence_penalty: 0,
     });
 
